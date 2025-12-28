8066668a552e0c33c60c3f71cbe80b6ed9f6a24a
mysterious461 <167347238+mysterious461@users.noreply.github.com>
2025-12-28 19:12:48 +0500

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,25 @@
-
 import 'package:flutter/material.dart';
-import 'features/journal/journal_page.dart';
-import 'core/theme/app_theme.dart';
 
-class GratitudeApp extends StatelessWidget {
+import 'core/theme/app_theme.dart';
+import 'features/home/home_page.dart';
+import 'features/journal/journal_page.dart';
+import 'features/calendar/calendar_page.dart';
+
+class GratitudeApp extends StatefulWidget {
   const GratitudeApp({super.key});
+
+  @override
+  State<GratitudeApp> createState() => _GratitudeAppState();
+}
+
+class _GratitudeAppState extends State<GratitudeApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomePage(),
+    JournalPage(),
+    CalendarPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +27,32 @@ class GratitudeApp extends StatelessWidget {
       title: 'Gratitude Daily',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: const JournalPage(),
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: _pages[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.edit),
+              label: 'Journal',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Calendar',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
